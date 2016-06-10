32bd6cb1580b0592461496cf510f686e42f03049
Ashley-Pettit <ashley@digital380.com.au>
2016-06-10 13:13:05 +1000

    adding tests

--- Olympics_api/app/controllers/event_controller.rb
+++ Olympics_api/app/controllers/event_controller.rb
@@ -1,7 +1,8 @@
 class EventController < ApplicationController
 
   # Countries/Sports have not been isolated into other Controllers/Models
-  # An assumption is made that the only data requiring creation is events and that this is the only object requiring CRUD functionality
+  # An assumption is made that the only data requiring creation is events and that this is the only object requiring CRUD functionality.
+  # Potentially this isn't best practice however, I decided class seperation wasn't warrented for this excercise.
 
   #view all => get '/event'
   def index
@@ -10,22 +11,22 @@ class EventController < ApplicationController
 
   # view one => get '/event/:id'
   def show
-    event = Event.find(params[:id])
+    event = Event.find_by_id(params[:id])
+    #if the event does not exist give error 404 and do not output JSON
     return status 404 if event.nil?
-    event.to_json
+    render json: event.to_json, status: 200
+  end
+
+  def search
+    search_input = params[:sport_search]
+    search_results = Event.find_each(sport_title: search_input)
+    render json: search_results.to_json, status: 200
   end
 
   # create => post '/events'
   def create
-    event = Event.new
-    event.sport_title = params[:sport_title]
-    event.countryA = params[:countryA]
-    event.countryB = params[:countryB]
-    event.start_time = params[:start_time]
-    event.end_time = params[:end_time]
-    event.date = params[:date]
-    event.countryAScore = params[:countryAScore]
-    event.countryBScore = params[:countryBScore]
+    #To whitelist params
+    Event.create(event_params)
     if event.save
       render json: event.to_json, status: 201
     else
@@ -36,15 +37,9 @@ class EventController < ApplicationController
   # update => patch '/events/:id'
   def update
     event = Event.find(params[:id])
+    event.update!(event_params)
     #if event not found send page not found error
     return status 404 if event.nil?
-    event.sport_title = params[:sport_title]
-    event.countryA = params[:countryA]
-    event.countryB = params[:countryB]
-    event.start_time = params[:start_time]
-    event.end_time = params[:end_time]
-    event.date = params[:date]
-    event.score = params[:score]
     if event.save
       #if saved then output saved data to JSON
       render json: event.to_json, status: 201
@@ -78,29 +73,24 @@ class EventController < ApplicationController
     event.countryAScore = event.countryBScore
   end
 
-  def search #This function is likely unnecessary
-    #This function shoud do the following
-    # - Display events which are coming up.
-    # - View the sports that are currently in progress.
-    # - Displays results from the previous games/races under the current sports.
-    # Eg. Basketball -> USA are currently playing Australia, 1hr ago Brazil defeated China 89-76, 2hrs ago Russia defeated Japan 102-101. Next game is USA vs Spain in 4 hours.
-    search_input = params[:sport_search]
-    @results = Event.find_by(sport_title: search_input) #Will this return 1 or more?
-    @output = ["#{search_input} ->"]
-    @results.each do |event|
-      if event.is_event_presently_running?
-        @output << "#{event.countryA} are current playing #{event.countryB},"
-      elsif event.is_event_in_past?
-        if !draw?
-          results = get_results
-          @output << "#{(Time.now - event.end_time).hour} hour(s) ago #{results.winning_country} defeated #{results.losing_country} #{results.winning_score}-#{results.losing_score},"
-        elsif
-          @output << "Draw Message"
-        end
-      elsif event.is_event_in_future?
-        @output << "Next game is #{event.countryA} vs #{event.countryB} in #{(Time.now - event.start_time).hour}"
-      end
-    end
+  # def search #Is this function necessary?
+  #   search_input = params[:sport_search]
+  #   @results = Event.find_each(sport_title: search_input)
+  #   @output = ["#{search_input} ->"]
+  #   @results.each do |event|
+  #     if event.is_event_presently_running?
+  #       @output << "#{event.countryA} are current playing #{event.countryB},"
+  #     elsif event.is_event_in_past?
+  #       if !draw?
+  #         results = get_results
+  #         @output << "#{(Time.now - event.end_time).hour} hour(s) ago #{results.winning_country} defeated #{results.losing_country} #{results.winning_score}-#{results.losing_score},"
+  #       elsif
+  #         @output << "Draw Message"
+  #       end
+  #     elsif event.is_event_in_future?
+  #       @output << "Next game is #{event.countryA} vs #{event.countryB} in #{(Time.now - event.start_time).hour}"
+  #     end
+  #   end
 
   def get_results
     if event.countryAScore > event.countryBScore
@@ -118,6 +108,12 @@ class EventController < ApplicationController
         losing_score: event.countryAScore
       }
     end
+  end
+
+  protected
+
+  def event_params
+    params.require(:sport_title].permit( :countryA, :countryB, :start_time, :end_time, :date:countryAScore, :countryBScore)
   end
 
 end
